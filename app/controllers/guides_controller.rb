103c1e7a4245afaa06d67d6cdcbaea52c0e58229
mtigvok <mrigvok@gmail.com>
2019-05-01 18:27:40 +0300

    User can create new guide

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,5 +1,17 @@
 class GuidesController < ApplicationController
+  before_action :authenticate_user!, except: [:show,:index]
+
   def new
+    @guide = Guide.new
+  end
+
+  def create
+    @guide = current_user.guides.build(guide_params)
+    if @guide.save
+      redirect_to guide_path(@guide.id)
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -9,5 +21,11 @@ class GuidesController < ApplicationController
   end
 
   def index
+  end
+
+  private
+
+  def guide_params
+    params.require(:guide).permit(:user_id, :title, :hero, :text)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'guides/new'
-  get 'guides/edit'
-  get 'guides/show'
-  get 'guides/index'
+
   devise_for :users
   root 'pages#index'
+  resources :guides
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
