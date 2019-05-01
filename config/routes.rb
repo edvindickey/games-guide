e6f09be9901d5862ce4bb33c0db83febf9ff66d7
mtigvok <mrigvok@gmail.com>
2019-05-01 21:01:42 +0300

    User can view guides in Category

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'categories/index'
-  get 'categories/show'
+
   devise_for :users
   root 'pages#index'
   resources :guides
+  resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
