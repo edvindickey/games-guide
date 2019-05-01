f3e2dd1b243b01801db1dda74f7aedf9f8dafbed
mtigvok <mrigvok@gmail.com>
2019-05-01 18:16:53 +0300

    create controller/model guide

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'guides/new'
+  get 'guides/edit'
+  get 'guides/show'
+  get 'guides/index'
   devise_for :users
   root 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
