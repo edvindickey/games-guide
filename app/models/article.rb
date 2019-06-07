cad78e76c23da195255f779f03b3b852c1d587e6
edvindickey <edvikon@gmail.com>
2019-06-07 11:29:52 +0300

    Add recommended news in show

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :categories
   has_many :comments
+  scope :random, -> {order('random()')}
 
   mount_uploaders :pictures, PictureUploader
   serialize :pictures, JSON
