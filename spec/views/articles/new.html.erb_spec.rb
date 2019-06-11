fbc0c26b73f660178c39de61c4e5169606bdee8f
orsandra89 <senusheugen@gmail.com>
2019-06-11 21:52:39 +0300

    Tests for Article routes

---
+++ spec/views/articles/new.html.erb_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "articles/new", type: :view do
+#  let!(:user) { create(:user) }
+
+#  before do
+#   @article = assign(:article, article.create!(title: "a", body: "b", user: user))
+#  end
+
+#  it "should rendered partial:'articles/form'" do
+#    allow(view).to receive(:current_user).and_return(user)
+#    stub_template "articles/_form.html.erb" => "Create new article"
+#    render
+#    expect(rendered).to match "Create new article"
+#  end
+
+
+
+#  it "should show form for 'Create new article'" do
+#    allow(view).to receive(:current_user).and_return(user)
+#    render
+#    expect(rendered).to include 'multipart/form-data'
+#  end
+
+end
