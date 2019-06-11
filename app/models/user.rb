ddbc9f2199206a4dd918e52038cc09103ba69068
orsandra89 <senusheugen@gmail.com>
2019-06-11 15:43:41 +0300

    Tests for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :guides
-         has_many :comments
-         has_many :articles
+         has_many :guides,   dependent: :destroy
+         has_many :comments, dependent: :destroy
+         has_many :articles, dependent: :destroy
 
 end
